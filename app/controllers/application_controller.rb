4f5fe03f497d7747b4943ecd1e78ff4d18e4ec39
Ivan Raszl <iraszl@gmail.com>
2019-03-14 01:33:08 +0800

    Fixed typo on method (#2)
    Reference: https://github.com/plataformatec/devise/wiki/How-To:-redirect-to-a-specific-page-on-successful-sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if params[:redirect_to].present?
       store_location_for(resource, params[:redirect_to])
-    elsif request.referer == new_session_url
+    elsif request.referer == new_user_session_url
       super
     else
       stored_location_for(resource) || request.referer || root_path
